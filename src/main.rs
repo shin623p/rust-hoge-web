8e8f2fc8b05093201a3a7576e182ba6bc8d2b8f5
Shinsuke <shinsuke@ssmba.local>
2017-06-17 07:34:18 +0900

    Init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
+        .http("localhost:3000")
+        .unwrap();
+}
