30df6c3a9ee773248eab618ecd3ed2160cfc8d56
shin623p <shin623p@gmail.com>
2017-07-01 03:14:07 +0900

    localhost -> 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ use iron::status;
 
 fn main() {
     Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
-        .http("localhost:3000")
+        .http("0.0.0.0:3000")
         .unwrap();
 }
