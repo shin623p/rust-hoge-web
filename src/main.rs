0569a8d5d6548d21062149a1ff321f3d6b756f66
shin623p <shin623p@gmail.com>
2017-07-13 16:59:19 +0900

    Add some function for primes

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,35 @@ fn gen_response(name: &String) -> String {
     json!({"text": "Hello, ".to_string() + name}).to_string()
 }
 
+fn is_prime(num: &u32) -> bool {
+    match *num {
+        1 => false,
+        2 => true,
+        _ => seive(num).contains(num),
+    }
+}
+
+fn seive(num: &u32) -> Vec<u32> {
+    let nums: Vec<u32> = (2..num + 1).collect::<Vec<_>>();
+    let primes: Vec<u32> = Vec::new();
+
+    fn go(mut p: Vec<u32>, mut n: Vec<u32>, num: &u32) -> Vec<u32> {
+        match n.first() {
+            Some(&x) if x * x <= *num => {
+                p.push(x);
+                n.retain(|&i| i % x != 0);
+                go(p, n, num)
+            }
+            _ => {
+                p.append(&mut n);
+                p
+            }
+        }
+    }
+
+    return go(primes, nums, num);
+}
+
 fn main() {
 
     fn top_handler(_: &mut Request) -> IronResult<Response> {
@@ -36,10 +65,28 @@ fn main() {
         }
     }
 
+    fn print_iter<'a, I: Iterator<Item = &'a u32>>(iter: I) {
+        // ここでは引数の"型"は明示せず、traitだけでいい
+        for v in iter {
+            println!("The iterator's value is {}", v);
+        }
+    }
+
+
+    println!("{:?}", seive(&100));
+    println!("{}", is_prime(&3));
+    println!("{}", is_prime(&1000000));
+    println!("{}", is_prime(&256));
+    println!("{}", is_prime(&25));
+
+
+    let hoge_iter: Vec<u32> = vec![1, 2, 3];
+    print_iter(hoge_iter.iter());
+
     let mut router = Router::new();
     router.get("/", top_handler, "top");
     router.post("/hoge", hoge_handler, "hoge");
 
-    Iron::new(router).http("0.0.0.0:3000").unwrap();
+    //Iron::new(router).http("0.0.0.0:3000").unwrap();
 
 }
